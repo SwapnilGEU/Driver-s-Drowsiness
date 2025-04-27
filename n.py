5e4c91524a0efdf557ccf22a5235292042303127
SwapnilGEU <159225133+SwapnilGEU@users.noreply.github.com>
2025-04-27 17:36:20 +0530

    n.py

--- n.py
+++ n.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import cv2
 import numpy as np
 from PIL import Image
 from ultralytics import YOLO  
@@ -21,23 +20,9 @@ if uploaded_file is not None:
     results = model.predict(img)
 
     # Get results image
-    result_img = results[0].plot()  # plots bounding boxes
+    result_img = results[0].plot()
 
     # Show result
     st.image(result_img, caption='Detected Image', use_column_width=True)
 
-video_file = st.file_uploader("Upload a Video...", type=["mp4", "mov", "avi"])
-
-if video_file is not None:
-    tfile = tempfile.NamedTemporaryFile(delete=False)
-    tfile.write(video_file.read())
-    cap = cv2.VideoCapture(tfile.name)
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        results = model.predict(frame)
-        result_frame = results[0].plot()
-        st.image(result_frame, channels="BGR")
 
